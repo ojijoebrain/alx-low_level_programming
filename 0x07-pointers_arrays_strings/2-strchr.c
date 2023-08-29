6750e131e6f53f6e7aee5003440fd5d2a07451ae
ojijoebrain <ojijosephoji@gmail.com>
2023-08-29 15:28:01 +0100

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string to be searched
+ * @c: the character to be searched for in the string
+ * Return: c or Null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (c == '\0')
+		return (s + i);
+
+	return (0);
+}
