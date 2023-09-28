1a1ad4da02085d8252919f20b46746a52b09ccaa
ojijoebrain <ojijosephoji@gmail.com>
2023-09-28 16:45:57 +0100

    print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary equll of a decimal num
+ * @n: to print num in binary
+ */
+void print_binary(unsigned long int n)
+{
+	int j, count = 0;
+	unsigned long int current;
+
+	for (j = 62; j >= 0; j--)
+	{
+		current = n >> j;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
+
+
