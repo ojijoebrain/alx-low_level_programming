608e811c2188d9d1d2c55237b60fbdfe25be9973
ojijoebrain <ojijosephoji@gmail.com>
2023-10-31 02:55:20 +0100

    creat_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: A pointer to  name of the file to created.
+ * @text_content: A pointer to  string to write to the file.
+ *
+ * Return:1 on success otherwise -1 on failure
+ *
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int pd, x, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	pd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(pd, text_content, len);
+
+	if (pd == -1 || x == -1)
+		return (-1);
+
+	close(pd);
+
+	return (1);
+}
+
+
+
+
